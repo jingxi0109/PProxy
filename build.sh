a18a3c43c7a1907fdb0abcd3a541c27d221be8aa
jingxi <jing.xi@hotmail.com>
2020-09-07 16:54:56 +0800

    ..

--- build.sh
+++ build.sh
@@ -1,2 +1,6 @@
 dotnet publish -c Release -o out -r linux-x64
 tar -zcvf  pub.tar.gz out/
+smbclient -U administrator%Developer200  //192.168.7.16/storfiles <<- EOF
+put /home/jingxi/Code/PProxy/pub.tar.gz  pub.tar.gz 
+exit
+EOF
